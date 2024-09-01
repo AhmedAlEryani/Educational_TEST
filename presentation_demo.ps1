7f84d156afc7c1b795dfa6cf21a2211685f26328
Exile <dota-2@live.com>
2024-09-01 23:00:56 +0200

    Update presentation_demo.ps1

--- presentation_demo.ps1
+++ presentation_demo.ps1
@@ -33,13 +33,13 @@ ___.                ____.     .__  .__                   _____                  
 #---------------------------------------------------------[Initialisations]--------------------------------------------------------
 
 # Directory Target to crypt 
-$TargetEncr = "C:\RanSim"
+$TargetEncr = "C:\Users\df018\Ransome_TEST"
 
 # At the end load CPU to triggered some behavior alarm 
 $CPULoad = $false
 
 # Delete the script ransomware.ps1 
-$SelfDestroy = $false
+$SelfDestroy = $true
 
 # Delete private key after 
 $DeleteKey = $true 
